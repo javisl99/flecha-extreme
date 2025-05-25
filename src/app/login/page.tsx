db67bbca3aef607aefce6748c80c19a55d95c38b
Javier Sánchez Lancha <javier.sanchez.lancha99@gmail.com>
2025-05-25 16:54:31 +0200

    Eliminación de la importación de Link en la página de inicio de sesión para simplificar el código.

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,7 +2,6 @@
 
 import { useState, useEffect } from 'react';
 import { Card, Button } from '@/shared/components';
-import Link from 'next/link';
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
 import { useUserContext } from '@/context/UserContext';
