69027177658ab107a681b461dacc775904347ce3
Javier Sánchez Lancha <javier.sanchez.lancha99@gmail.com>
2025-05-25 16:54:41 +0200

    Eliminación de importaciones innecesarias de clientesMock y reservasMock en el archivo data.ts para simplificar el código.

--- src/components/Pagos/data.ts
+++ src/components/Pagos/data.ts
@@ -1,5 +1,3 @@
 import { Pago } from '@/shared/types';
-import { clientesMock } from '@/components/Clientes/data';
-import { reservasMock } from '@/components/Reservas/data';
 
 export const pagosMock: Pago[] = []; 
